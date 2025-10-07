7bc02f53d2b9b132d69f283221970e484cbf6bcb
Barbara Locsi <locsi.barbara@gmail.com>
2025-10-07 21:51:01 +0200

    Add separate console writer class

---
+++ ConsoleWriter/MyConsoleObserver.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleWriter;
+
+public class MyConsoleObserver<T> : IObserver<T>
+{
+    public void OnNext(T value)
+    {
+        Console.WriteLine($"Received value {value}");
+    }
+
+    public void OnError(Exception error)
+    {
+        Console.WriteLine($"Sequence faulted with {error}");
+    }
+
+    public void OnCompleted()
+    {
+        Console.WriteLine("Sequence terminated");
+    }
+}
---
+++ ConsoleWriter/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reactive.Linq;
+using ConsoleWriter;
+
+Console.WriteLine("Hello, World!");
+
+var observer = new MyConsoleObserver<long>();
+
+IObservable<long> ticks = Observable.Timer(
+    dueTime: TimeSpan.Zero,
+    period: TimeSpan.FromSeconds(1));
+    
+ticks.Subscribe(observer);
+
+Console.ReadLine();
