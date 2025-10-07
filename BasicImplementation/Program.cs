f7f3396bbead5f76ef5478700cb5a89d2e6a3c68
Barbara Locsi <locsi.barbara@gmail.com>
2025-10-07 23:56:40 +0200

    Add basic implementation

---
+++ BasicImplementation/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BasicImplementation;
+
+Console.WriteLine("Hello, World!");
+
+
+var numbers = new MySequenceOfNumbers();
+numbers.Subscribe(
+    number => Console.WriteLine($"Received value: {number}"),
+    () => Console.WriteLine("Sequence terminated"));
+    
+    
+Console.Read();
