e681614622a8cd68c3ef712484f2cb6265ede4b8
Barbara Locsi <locsi.barbara@gmail.com>
2025-10-08 22:36:48 +0200

    Add multi subscriber file system example

--- src/05_FileSystemExample/Program.cs
+++ src/05_FileSystemExample/Program.cs
@@ -6,18 +6,42 @@ using FileSystemExample;
 
 Console.WriteLine("Hello, World!");
 
-var fs = new RxFsEvents("C:\\Barbi\\TryRx\\src\\05_FileSystemExample\\test");
 
-IObservable<FileSystemEventArgs> configChanges =
-    fs.Where(e => Path.GetExtension(e.Name) == ".config");
-
-IObservable<FileSystemEventArgs> deletions =
-    fs.Where(e => e.ChangeType == WatcherChangeTypes.Deleted);
-    
-fs.Subscribe(
-    value => Console.WriteLine($"Inline - Received value {JsonSerializer.Serialize(value)}"),
-    error => Console.WriteLine($"Inline - Sequence faulted with {JsonSerializer.Serialize(error)}"),
-    () => Console.WriteLine("Inline - Sequence terminated")
-);
+//OversimplifiedExample();
+MultiSubscriberExample();
 
 Console.ReadLine();
+
+void OversimplifiedExample()
+{
+    var fs = new RxFsEvents("C:\\Barbi\\TryRx\\src\\05_FileSystemExample\\test");
+
+    fs.Subscribe(
+        value => Console.WriteLine($"Inline - Received value {JsonSerializer.Serialize(value)}"),
+        error => Console.WriteLine($"Inline - Sequence faulted with {JsonSerializer.Serialize(error)}"),
+        () => Console.WriteLine("Inline - Sequence terminated")
+    );
+}
+
+void MultiSubscriberExample()
+{
+    var fs = new RxFsEventsMultiSubscriber("C:\\Barbi\\TryRx\\src\\05_FileSystemExample\\test");
+    
+    IObservable<FileSystemEventArgs> configChanges =
+        fs.Where(e => Path.GetExtension(e.Name) == ".config");
+
+    IObservable<FileSystemEventArgs> deletions =
+        fs.Where(e => e.ChangeType == WatcherChangeTypes.Deleted);
+    
+    configChanges.Subscribe(
+        value => Console.WriteLine($"Inline Config - Received value {JsonSerializer.Serialize(value)}"),
+        error => Console.WriteLine($"Inline Config - Sequence faulted with {JsonSerializer.Serialize(error)}"),
+        () => Console.WriteLine("Inline Config - Sequence terminated")
+    );
+    
+    deletions.Subscribe(
+        value => Console.WriteLine($"Inline deletion - Received value {JsonSerializer.Serialize(value)}"),
+        error => Console.WriteLine($"Inline deletion - Sequence faulted with {JsonSerializer.Serialize(error)}"),
+        () => Console.WriteLine("Inline deletion - Sequence terminated")
+    );
+}
---
+++ src/05_FileSystemExample/RxFsEventsMultiSubscriber.cs
@@ -0,0 +1,97 @@
+﻿namespace FileSystemExample;
+
+public class RxFsEventsMultiSubscriber : IObservable<FileSystemEventArgs>
+{
+    private readonly object sync = new();
+    private readonly List<Subscription> subscribers = new();
+    private readonly FileSystemWatcher watcher;
+
+    public RxFsEventsMultiSubscriber(string folder)
+    {
+        this.watcher = new FileSystemWatcher(folder);
+
+        watcher.Created += SendEventToObservers;
+        watcher.Changed += SendEventToObservers;
+        watcher.Renamed += SendEventToObservers;
+        watcher.Deleted += SendEventToObservers;
+
+        watcher.Error += SendErrorToObservers;
+    }
+
+    public IDisposable Subscribe(IObserver<FileSystemEventArgs> observer)
+    {
+        Subscription sub = new(this, observer);
+        lock (this.sync)
+        {
+            this.subscribers.Add(sub); 
+
+            if (this.subscribers.Count == 1)
+            {
+                // We had no subscribers before, but now we've got one so we need
+                // to start up the FileSystemWatcher.
+                watcher.EnableRaisingEvents = true;
+            }
+        }
+
+        return sub;
+    }
+
+    private void Unsubscribe(Subscription sub)
+    {
+        lock (this.sync)
+        {
+            this.subscribers.Remove(sub);
+
+            if (this.subscribers.Count == 0)
+            {
+                watcher.EnableRaisingEvents = false;
+            }
+        }
+    }
+
+    void SendEventToObservers(object _, FileSystemEventArgs e)
+    {
+        lock (this.sync)
+        {
+            foreach (var subscription in this.subscribers)
+            {
+                subscription.Observer.OnNext(e);
+            }
+        }
+    }
+
+    void SendErrorToObservers(object _, ErrorEventArgs e)
+    {
+        Exception x = e.GetException();
+        lock (this.sync)
+        {
+            foreach (var subscription in this.subscribers)
+            {
+                subscription.Observer.OnError(x);
+            }
+
+            this.subscribers.Clear();
+        }
+    }
+
+    private class Subscription : IDisposable
+    {
+        private RxFsEventsMultiSubscriber? parent;
+
+        public Subscription(
+            RxFsEventsMultiSubscriber rxFsEventsMultiSubscriber,
+            IObserver<FileSystemEventArgs> observer)
+        {
+            this.parent = rxFsEventsMultiSubscriber;
+            this.Observer = observer;
+        }
+        
+        public IObserver<FileSystemEventArgs> Observer { get; }
+
+        public void Dispose()
+        {
+            this.parent?.Unsubscribe(this);
+            this.parent = null;
+        }
+    }
+}
