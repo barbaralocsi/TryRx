04122c40fa4fc43b6fd8c1bdc24fb66e4435588a
Barbara Locsi <locsi.barbara@gmail.com>
2025-10-08 21:40:01 +0200

    Add file system example

---
+++ FileSystemExample/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reactive.Linq;
+using System.Text.Json;
+using FileSystemExample;
+
+Console.WriteLine("Hello, World!");
+
+var fs = new RxFsEvents("C:\\Barbi\\TryRx\\FileSystemExample\\test");
+
+IObservable<FileSystemEventArgs> configChanges =
+    fs.Where(e => Path.GetExtension(e.Name) == ".config");
+
+IObservable<FileSystemEventArgs> deletions =
+    fs.Where(e => e.ChangeType == WatcherChangeTypes.Deleted);
+    
+fs.Subscribe(
+    value => Console.WriteLine($"Inline - Received value {JsonSerializer.Serialize(value)}"),
+    error => Console.WriteLine($"Inline - Sequence faulted with {JsonSerializer.Serialize(error)}"),
+    () => Console.WriteLine("Inline - Sequence terminated")
+);
+
+Console.ReadLine();
