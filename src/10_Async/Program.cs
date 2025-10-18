481c3dff115a98874b23f1d7d91c3cecd77d0f8d
Barbara Locsi <locsi.barbara@gmail.com>
2025-10-18 18:03:07 +0200

    Try async

---
+++ src/10_Async/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Threading.Tasks;
+
+// This is not based on the book, but based on a github issue: https://github.com/dotnet/reactive/issues/459
+
+Observable.Interval(TimeSpan.FromSeconds(1))
+    .SubscribeAsync(number => DoSomeWorkAsync(number));
+
+Console.ReadLine();
+
+async Task DoSomeWorkAsync(long number)
+{
+    Console.WriteLine($"DoSomeWorkAsync BEGIN '{number}'");
+    await Task.Delay(TimeSpan.FromSeconds(3));
+    Console.WriteLine($"DoSomeWorkAsync END '{number}'");
+}
+
+#if false
+public static class MyObservableExtensions
+{
+    public static IDisposable SubscribeAsync<T>(this IObservable<T> source, Func<T, Task> onNextAsync) =>
+        source
+            .Select(number => Observable.FromAsync(() => onNextAsync(number)))
+            .Concat()
+            .Subscribe();
+
+    public static IDisposable SubscribeAsyncConcurrent<T>(this IObservable<T> source, Func<T, Task> onNextAsync) =>
+        source
+            .Select(number => Observable.FromAsync(() => onNextAsync(number)))
+            .Merge()
+            .Subscribe();
+
+    public static IDisposable SubscribeAsyncConcurrent<T>(this IObservable<T> source, Func<T, Task> onNextAsync, int maxConcurrent) =>
+        source
+            .Select(number => Observable.FromAsync(() => onNextAsync(number)))
+            .Merge(maxConcurrent)
+            .Subscribe();
+}
+#else
+public static class MyObservableExtensions
+{
+    public static IDisposable SubscribeAsync<T>(this IObservable<T> source, Func<T, Task> onNextAsync) =>
+        source
+            .Select(number => onNextAsync(number).ToObservable()) // note ToObservable instead of FromAsync!
+            .Concat()
+            .Subscribe();
+
+    public static IDisposable SubscribeAsyncConcurrent<T>(this IObservable<T> source, Func<T, Task> onNextAsync) =>
+        source
+            .Select(number => onNextAsync(number).ToObservable()) // note ToObservable instead of FromAsync!
+            .Merge()
+            .Subscribe();
+
+    public static IDisposable SubscribeAsyncConcurrent<T>(this IObservable<T> source, Func<T, Task> onNextAsync, int maxConcurrent) =>
+        source
+            .Select(number => onNextAsync(number).ToObservable()) // note ToObservable instead of FromAsync!
+            .Merge(maxConcurrent)
+            .Subscribe();
+}
+#endif
